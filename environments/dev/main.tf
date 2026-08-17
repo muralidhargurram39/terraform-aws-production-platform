536653fb58bcf125e19c4ade720bd519bb70b8b3
muralidhargurram39 <muralidharaws39@gmail.com>
2026-08-17 07:08:05 +0000

    refactor: move dev DNS ownership to global route53

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -144,16 +144,3 @@ module "compute" {
 
   tags = local.common_tags
 }
-
-resource "aws_route53_record" "dev" {
-  zone_id = data.terraform_remote_state.route53.outputs.zone_id
-
-  name = "dev.muralidharops.com"
-  type = "A"
-
-  alias {
-    name                   = module.compute.alb_dns_name
-    zone_id                = module.compute.alb_zone_id
-    evaluate_target_health = true
-  }
-}
