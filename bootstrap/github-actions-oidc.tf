9f3088f3437ca4b6ff91c80ee6cf3e13d3cb9f11
muralidhargurram39 <muralidharaws39@gmail.com>
2026-08-25 06:08:06 +0000

    fix(ci): add S3 discovery permissions for Terraform

--- bootstrap/github-actions-oidc.tf
+++ bootstrap/github-actions-oidc.tf
@@ -215,6 +215,36 @@ resource "aws_iam_policy" "github_actions_terraform_ci" {
         Resource = "*"
       },
 
+      {
+        Sid    = "S3Discovery"
+        Effect = "Allow"
+
+        Action = [
+          "s3:GetBucketAcl",
+          "s3:GetBucketCORS",
+          "s3:GetBucketLocation",
+          "s3:GetBucketLogging",
+          "s3:GetBucketNotification",
+          "s3:GetBucketOwnershipControls",
+          "s3:GetBucketPolicy",
+          "s3:GetBucketPolicyStatus",
+          "s3:GetBucketPublicAccessBlock",
+          "s3:GetBucketRequestPayment",
+          "s3:GetBucketTagging",
+          "s3:GetBucketVersioning",
+          "s3:GetBucketWebsite",
+          "s3:GetEncryptionConfiguration",
+          "s3:GetLifecycleConfiguration",
+          "s3:GetReplicationConfiguration",
+          "s3:GetAccelerateConfiguration",
+          "s3:GetObjectLockConfiguration",
+          "s3:GetBucketIntelligentTieringConfiguration",
+          "s3:ListBucket"
+        ]
+
+        Resource = "*"
+      },
+
       # ----------------------------------------------
       # Elastic Load Balancing v2 Discovery
       #
